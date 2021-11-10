0acc2f9df20c52f9dab5b4f502b5fcdc4d9dd707
DESKTOP-98QCCGN\arvin <arvinohanian@gmail.com>
2021-11-10 15:12:37 -0800

    teting

---
+++ Layer.py
@@ -0,0 +1,10 @@
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.output=None
+        
+    def forward_propagation(self, input):
+        
+        
+    def back_propagation(self, error, learning_rate):
+        some change for test
